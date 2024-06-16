3a9121145a641d4496eac8485ed39fc5be6fe811
Emmanuel Toko <emmanuel.toko@gmail.com>
2024-06-16 19:15:58 +0300

    Initial commit

---
+++ 02 Machine Learning with Python/Projects/Rock Paper Scissors/RPS.py
@@ -0,0 +1,10 @@
+# The example function below keeps track of the opponent's history 
+
+def player(prev_play, opponent_history=[]):
+    opponent_history.append(prev_play)
+
+    guess = "R"
+    if len(opponent_history) > 2:
+        guess = opponent_history[-2]
+
+    return guess
